a88913095227dbfa7c365d22fbb2f04a76c93fca
Mod617 <hounvosegbede@gmail.com>
2026-09-10 12:34:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6525,7 +6525,7 @@ def envoyer_email_contact_async(app, contact_msg_id):
 
 
 @app.route("/contact", methods=["GET", "POST"])
-@limiter.limit("5 per hour", methods=["POST"])
+@limiter.limit("20 per hour", methods=["POST"])
 def contact():
     if request.method == "POST":
         name = bleach.clean(request.form.get("name", "").strip())
